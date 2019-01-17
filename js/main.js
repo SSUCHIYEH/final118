3d78f8585d3e5e6acd62a367bd44ec4c4da03460
tsaiyuyes7 <39110981+tsaiyuyes7@users.noreply.github.com>
2019-01-17 19:55:50 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+   $(".showbig1").hide(); 
+    $(".news1img .close").click(function(event){
+        $(".showbig1").hide();
+    });
+    $(".news1title .close2").click(function(event){
+        $(".showbig1").hide();
+    });
+    $(".news1").click(function(event){
+        $(".showbig1").show();
+    });
+
+    $(".showbig2").hide();
+    $(".news2img .close").click(function(event){
+        $(".showbig2").hide();
+    });
+    $(".news2title .close2").click(function(event){
+        $(".showbig2").hide();
+    });
+    $(".news2 ").click(function(event){
+        $(".showbig2").show();
+    });
+
+    $(".showbig3").hide();
+    $(".news3img .close").click(function(event){
+        $(".showbig3").hide();
+    });
+    $(".news3title .close2").click(function(event){
+        $(".showbig3").hide();
+    });
+    $(".news3 ").click(function(event){
+        $(".showbig3").show();
+    });
+    $(".showbig4").hide();
+    $(".news4img .close").click(function(event){
+        $(".showbig4").hide();
+    });
+    $(".news4title .close2").click(function(event){
+        $(".showbig4").hide();
+    });
+    $(".news4 ").click(function(event){
+        $(".showbig4").show();
+    });
+    $(".showbig5").hide(); 
+    $(".news5img .close").click(function(event){
+        $(".showbig5").hide();
+    });
+    $(".news5title .close2").click(function(event){
+        $(".showbig5").hide();
+    });
+    $(".news5 ").click(function(event){
+        $(".showbig5").show();
+    });
+});
